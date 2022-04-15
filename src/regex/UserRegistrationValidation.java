b69b6cb79de645314b07cc300c3b20dd59aefdf8
unknown <SmithaRai.com>
2022-04-15 20:50:21 +0530

    Added : added println statememt

---
+++ src/regex/UserRegistrationValidation.java
@@ -0,0 +1,11 @@
+package regex;
+
+public class UserRegistrationValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("*********USER REGISTERATION VALIDATION**************");
+	}
+
+}
